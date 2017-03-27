0524f65cd8cb342ce2f6c374917e97d4d356ee94
SNMetamorph <vova28rus@gmail.com>
2017-03-27 15:58:59 +0900

    Update SimpleLED.h

--- src/SimpleLED.h
+++ src/SimpleLED.h
@@ -3,17 +3,28 @@
 #define SL_GREEN 2
 #define SL_BLUE 3
 
+struct SL_RGB 
+{
+	byte r, g, b;
+};
+
+struct SL_PIN 
+{
+	int r, g, b;
+};
+
 class SimpleLED 
 {
     public:            
-      SimpleLED(int arr[3]);
+      SimpleLED(int r, int g, int b);
       void SetColor(byte r, byte g, byte b);
+	  bool SetDefColor(int num);
       void SetHSVColor(float h, float s, float v);
+	  SL_RGB GetColor();
       void SetBrightness(float value);
       void Flush();
-      bool SetDefColor(int num);
-    private:
-      int ledarray[3];
+	private:
       float brightness;
-      byte backcol[3];
+	  SL_PIN pins;
+      SL_RGB backcol = {0, 0, 0};
 };
